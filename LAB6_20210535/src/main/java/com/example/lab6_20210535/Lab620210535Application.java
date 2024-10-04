5985027e3fde7f51ed6872d13bc2006e245c50a0
RCT4H3 <139926077+RCT4H3@users.noreply.github.com>
2024-10-03 19:53:01 -0500

    Creacion del proyecto lab6

---
+++ LAB6_20210535/src/main/java/com/example/lab6_20210535/Lab620210535Application.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20210535;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab620210535Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab620210535Application.class, args);
+    }
+
+}
---
+++ LAB6_20210535/src/test/java/com/example/lab6_20210535/Lab620210535ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_20210535;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab620210535ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
